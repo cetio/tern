2262c21f6f4be36b0a9e05b56bd75cb74cbd32c3
Mitchell <80652149+HeronErin@users.noreply.github.com>
2024-02-28 15:54:10 +0000

    Atomic variable test

--- source/tests/atomic.d
+++ source/tests/atomic.d
@@ -1,6 +1,6 @@
 module tests.atomic;
 import tern.atomic;
-
+import std.parallelism;
 unittest
 {
     shared int val = 10;
@@ -23,4 +23,26 @@ unittest
 {
     shared int val = 10;
     assert(val.atomicOp!"+"(1) == 11);
+}
+
+
+
+// This is THE worst way to use atomic variables!
+shared ulong count = 0;
+void addUp(){
+    foreach(_ ; 0..1_000_000){
+        atomicOp!"+="(count, 1);
+    } 
+}  
+
+unittest
+{
+ 
+    auto pool = new TaskPool(75);
+    foreach(_; 0..150){
+        pool.put(task!addUp());
+    }
+    pool.finish(true);
+
+    assert(count.atomicLoad == 150 * 1_000_000);
 }
